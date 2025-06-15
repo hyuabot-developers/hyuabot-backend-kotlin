8b8665f83a7b0e70591b7d9984d57bf553004483
Jeongin Lee <jil8885@gmail.com>
2025-06-15 21:47:06 +0900

    fix: Remove generated value config

--- src/main/kotlin/app/hyuabot/backend/database/entity/RefreshToken.kt
+++ src/main/kotlin/app/hyuabot/backend/database/entity/RefreshToken.kt
@@ -2,8 +2,6 @@ package app.hyuabot.backend.database.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
@@ -16,7 +14,6 @@ import java.util.UUID
 data class RefreshToken(
     @Id
     @Column(name = "uuid", columnDefinition = "uuid")
-    @GeneratedValue(strategy = GenerationType.UUID)
     val uuid: UUID,
     @Column(name = "user_id", length = 20, nullable = false)
     val userID: String,
