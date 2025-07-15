98d84d0a559c9fe7acd8aaca9e436aeea7d0b186
Jeongin Lee <jil8885@gmail.com>
2025-07-15 18:34:57 +0900

    fix: Remove kotlin assert and use assertEquals instead

--- src/test/kotlin/app/hyuabot/backend/utility/ResponseBuilderTest.kt
+++ src/test/kotlin/app/hyuabot/backend/utility/ResponseBuilderTest.kt
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseCookie
+import kotlin.test.assertEquals
 
 class ResponseBuilderTest {
     @Test
-    @DisplayName("Test response with message amd cookies")
+    @DisplayName("Test response with message and cookies")
     fun createResponseWithCookies() {
         val response =
             ResponseBuilder.response(
@@ -29,13 +30,13 @@ class ResponseBuilderTest {
                             .build(),
                     ),
             )
-        assert(response.statusCode == HttpStatus.OK)
-        assert(response.body?.message == "Success")
+        assertEquals(response.statusCode, HttpStatus.OK)
+        assertEquals(response.body?.message, "Success")
         response.headers.forEach {
             if (it.key == "Set-Cookie") {
-                assert(it.value.size == 2)
-                assert(it.value[0].contains("accessToken"))
-                assert(it.value[1].contains("refreshToken"))
+                assertEquals(it.value.size, 2)
+                assertEquals(it.value[0].contains("accessToken"), true)
+                assertEquals(it.value[1].contains("refreshToken"), true)
             }
         }
     }
@@ -44,26 +45,26 @@ class ResponseBuilderTest {
     @DisplayName("Test response with message only")
     fun createResponseWithMessage() {
         val response = ResponseBuilder.response(HttpStatus.OK, message = "Operation successful")
-        assert(response.statusCode == HttpStatus.OK)
-        assert(response.body?.message == "Operation successful")
-        assert(response.headers.contentType?.toString() == "application/json")
+        assertEquals(response.statusCode, HttpStatus.OK)
+        assertEquals(response.body?.message, "Operation successful")
+        assertEquals(response.headers.contentType?.toString(), "application/json")
     }
 
     @Test
     @DisplayName("Test response with body")
     fun createResponseWithBody() {
         val response = ResponseBuilder.response(HttpStatus.OK, body = mapOf("key" to "value"))
-        assert(response.statusCode == HttpStatus.OK)
-        assert(response.body is Map<*, *>)
-        assert((response.body as Map<*, *>)["key"] == "value")
-        assert(response.headers.contentType?.toString() == "application/json")
+        assertEquals(response.statusCode, HttpStatus.OK)
+        assertEquals(response.body is Map<*, *>, true)
+        assertEquals((response.body as Map<*, *>)["key"], "value")
+        assertEquals(response.headers.contentType?.toString(), "application/json")
     }
 
     @Test
     @DisplayName("Test exception response")
     fun createExceptionResponse() {
         val exception = ResponseBuilder.exception(HttpStatus.BAD_REQUEST, "Bad Request")
-        assert(exception.statusCode == HttpStatus.BAD_REQUEST)
-        assert(exception.reason == "Bad Request")
+        assertEquals(exception.statusCode, HttpStatus.BAD_REQUEST)
+        assertEquals(exception.reason, "Bad Request")
     }
 }
