502b6d9a7a2a6af390604f8a30d9941f557b4aa9
Jeongin Lee <jil8885@gmail.com>
2025-07-27 22:00:57 +0900

    feat: Implement room related API

--- src/main/kotlin/app/hyuabot/backend/room/RoomService.kt
+++ src/main/kotlin/app/hyuabot/backend/room/RoomService.kt
@@ -1,6 +1,8 @@
 package app.hyuabot.backend.room
 
+import app.hyuabot.backend.building.exception.BuildingNotFoundException
 import app.hyuabot.backend.database.entity.Room
+import app.hyuabot.backend.database.repository.BuildingRepository
 import app.hyuabot.backend.database.repository.RoomRepository
 import app.hyuabot.backend.room.domain.RoomRequest
 import app.hyuabot.backend.room.exception.DuplicateRoomException
@@ -9,16 +11,25 @@ import org.springframework.stereotype.Service
 
 @Service
 class RoomService(
+    private val buildingRepository: BuildingRepository,
     private val roomRepository: RoomRepository,
 ) {
-    fun getRoomList(buildingName: String?): List<Room> =
-        buildingName?.let {
-            roomRepository.findByNameContaining(it)
-        } ?: roomRepository.findAll()
+    fun getRoomList(
+        buildingName: String?,
+        name: String?,
+    ): List<Room> =
+        when {
+            buildingName != null && name != null -> roomRepository.findByBuildingNameAndNameContaining(buildingName, name)
+            buildingName != null -> roomRepository.findByBuildingName(buildingName)
+            name != null -> roomRepository.findByNameContaining(name)
+            else -> roomRepository.findAll()
+        }.sortedBy { it.seq }
 
     fun getRoomByID(seq: Int): Room = roomRepository.findById(seq).orElseThrow { RoomNotFoundException() }
 
     fun createRoom(payload: RoomRequest): Room {
+        buildingRepository.findByName(payload.buildingName)
+            ?: throw BuildingNotFoundException()
         roomRepository
             .findByBuildingNameAndNumber(
                 buildingName = payload.buildingName,
@@ -39,13 +50,16 @@ class RoomService(
     fun updateRoom(
         seq: Int,
         payload: RoomRequest,
-    ): Room =
-        roomRepository.findById(seq).orElseThrow { RoomNotFoundException() }.let { room ->
+    ): Room {
+        buildingRepository.findByName(payload.buildingName)
+            ?: throw BuildingNotFoundException()
+        return roomRepository.findById(seq).orElseThrow { RoomNotFoundException() }.let { room ->
             room.buildingName = payload.buildingName
             room.number = payload.number
             room.name = payload.name
             roomRepository.save(room)
         }
+    }
 
     fun deleteRoom(seq: Int) {
         roomRepository.findById(seq).orElseThrow { RoomNotFoundException() }.let { room ->
