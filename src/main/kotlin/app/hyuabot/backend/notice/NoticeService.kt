9d65ccce2bdba00fc9df68f33e9365ed4522c8a1
Jeongin Lee <jil8885@gmail.com>
2026-03-01 19:47:50 +0900

    fix: Code Convention

--- src/main/kotlin/app/hyuabot/backend/notice/NoticeService.kt
+++ src/main/kotlin/app/hyuabot/backend/notice/NoticeService.kt
@@ -133,10 +133,11 @@ class NoticeService(
             .filter { cat ->
                 category == null || cat.name.contains(category, ignoreCase = true)
             }.map { cat ->
-                val filteredNotices = cat.notice
-                    .filter { n ->
-                        (language == null || n.language == language) && n.expiredAt.isAfter(timestamp)
-                    }.toMutableList()
+                val filteredNotices =
+                    cat.notice
+                        .filter { n ->
+                            (language == null || n.language == language) && n.expiredAt.isAfter(timestamp)
+                        }.toMutableList()
                 cat.copy(notice = filteredNotices)
             }
     }
