5f8c5f4f89b077f740c825dab2b343c4ea8ead0a
Jeongin Lee <jil8885@gmail.com>
2026-03-12 20:42:19 +0900

    feat: Implement GraphQL schema for reading room

---
+++ src/main/kotlin/app/hyuabot/backend/readingRoom/ReadingRoomDataFetcher.kt
@@ -0,0 +1,34 @@
+package app.hyuabot.backend.readingRoom
+
+import app.hyuabot.backend.codegen.types.ReadingRoom
+import app.hyuabot.backend.codegen.types.ReadingRoomSeat
+import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsQuery
+
+@DgsComponent
+class ReadingRoomDataFetcher(
+    private val readingRoomService: ReadingRoomService,
+) {
+    @DgsQuery
+    fun readingRoom(): List<ReadingRoom> =
+        readingRoomService
+            .getReadingRoomList(
+                null,
+            ).map { readingRoom ->
+                ReadingRoom(
+                    seq = readingRoom.id,
+                    name = readingRoom.name,
+                    campus = readingRoom.campusID,
+                    active = readingRoom.isActive,
+                    reservable = readingRoom.isReservable,
+                    seats =
+                        ReadingRoomSeat(
+                            total = readingRoom.total,
+                            available = readingRoom.available,
+                            active = readingRoom.active,
+                            occupied = readingRoom.occupied,
+                        ),
+                    updatedAt = readingRoom.updatedAt,
+                )
+            }
+}
